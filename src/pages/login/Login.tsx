db8a40d2cd64e4b3cea1e1cd5e481525f5d43f2b
Arne Tesch <arnetesch@gmail.com>
2019-11-17 18:51:46 +0100

    initial commit - basic setup

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Login.scss";
+
+const Login: React.FC = () => {
+  return (
+    <div className="container">
+      <div className="auth-form">
+        <h1>LOGIN</h1>
+      </div>
+    </div>
+  );
+};
+export default Login;
