1b8cbfe3cf10a7a866b343717e18c49da2fe63e3
Arne Tesch <arnetesch@gmail.com>
2019-11-18 18:51:31 +0100

    auth screen setup

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -3,9 +3,36 @@ import "./Login.scss";
 
 const Login: React.FC = () => {
   return (
-    <div className="container">
-      <div className="auth-form">
-        <h1>LOGIN</h1>
+    <div className="page-wrapper">
+      <div className="container">
+        <div className="signup">
+          <img
+            src={`${window.location.origin}/assets/logo.png`}
+            srcSet={`${window.location.origin}/assets/logo@2x.png`}
+            alt="logo"
+          />
+          <h2>Welcome!</h2>
+          <p>Let's Create your Account</p>
+          <button>
+            <span>Sign up</span>
+          </button>
+        </div>
+        <div className="login">
+          <h1>Welcome Back!</h1>
+          <form className="login-form">
+            <div className="form-control">
+              <label htmlFor="e-mail">E-mail:</label>
+              <input type="text" />
+            </div>
+            <div className="form-control">
+              <label htmlFor="e-mail">Password:</label>
+              <input type="password" />
+            </div>
+            <button>
+              <span>Login</span>
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
