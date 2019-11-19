1d4d8185fab2cc4db13a755ed96a3bc0a99e4b8b
Arne Tesch <arnetesch@gmail.com>
2019-11-19 19:01:00 +0100

    Login query and form validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.scss";
 import Login from "./pages/login/Login";
 
+const client = new ApolloClient({
+  uri: "http://localhost:8000/graphql"
+});
+
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/login" component={Login} />
-      </Switch>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" component={Login} />
+        </Switch>
+      </BrowserRouter>
+    </ApolloProvider>
   );
 };
 
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,7 +1,65 @@
+import { useLazyQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
 import React from "react";
+import useForm from "react-hook-form";
+import * as yup from "yup";
 import "./Login.scss";
 
+type FormData = {
+  email: string;
+  password: string;
+};
+
+const LoginSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Please provide a valid e-mailadress.")
+    .required("E-mail is a required field."),
+  password: yup.string().required("Password is a required field.")
+});
+
+const LOGIN = gql`
+  query adminLogin($email: String!, $password: String!) {
+    adminLogin(email: $email, password: $password) {
+      userId
+      token
+      tokenExpiration
+    }
+  }
+`;
+
 const Login: React.FC = () => {
+  const { register, errors, handleSubmit } = useForm<FormData>({
+    validationSchema: LoginSchema
+  });
+  const [login, { loading, data }] = useLazyQuery(LOGIN);
+
+  if (loading) {
+    return <p>LOADING...</p>;
+  }
+
+  if (data && data.adminLogin) {
+    localStorage.setItem("auth_token", data.adminLogin.token);
+  }
+
+  const onSubmit = handleSubmit(({ email, password }) => {
+    console.log({ email }, { password });
+
+    if (
+      (!email && email.trim().length === 0) ||
+      (!password && password.trim().length === 0)
+    ) {
+      return;
+    }
+
+    login({
+      variables: {
+        email: email,
+        password: password
+      }
+    });
+  });
+
   return (
     <div className="page-wrapper">
       <div className="container">
@@ -12,21 +70,31 @@ const Login: React.FC = () => {
             alt="logo"
           />
           <h2>Welcome!</h2>
-          <p>Let's Create your Account</p>
+          <p>Let's Connect</p>
           <button>
             <span>Sign up</span>
           </button>
         </div>
         <div className="login">
           <h1>Welcome Back!</h1>
-          <form className="login-form">
+          <form className="login-form" onSubmit={onSubmit}>
             <div className="form-control">
-              <label htmlFor="e-mail">E-mail:</label>
-              <input type="text" />
+              <label htmlFor="email">E-mail:</label>
+              <input type="text" name="email" ref={register} />
+              <span className="form-error-msg">
+                {errors.email && errors.email.message}
+              </span>
             </div>
             <div className="form-control">
-              <label htmlFor="e-mail">Password:</label>
-              <input type="password" />
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                name="password"
+                ref={register({ required: "This field is required" })}
+              />
+              <span className="form-error-msg">
+                {errors.password && errors.password.message}
+              </span>
             </div>
             <button>
               <span>Login</span>
